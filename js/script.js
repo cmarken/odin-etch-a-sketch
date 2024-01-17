897303d9c8b724dc6c887a8d130c7c2944ca42b5
Chris Marken <11192801+cmarken@users.noreply.github.com>
2024-01-17 20:28:30 +0100

    Draw grid on load and trigger on mouse leave

--- js/script.js
+++ js/script.js
@@ -14,11 +14,28 @@
             // add the event listener "mouseleave" to pixel to execute onLeaveColorPixel()
             // add pixel to the row
         // add row to the component div
+function drawGrid(x) {
+    const pixelGrid = document.querySelector('pixel-grid');
+    for (let i = 1; i <= x; i++) {
+        const row = document.createElement('div');
+        row.classList.add('pixel-row');
+        for (let j = 1; j <= x; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseleave', onLeaveColorPixel);
+            row.appendChild(pixel);
+        }
+        pixelGrid.appendChild(row);
+    }
+}
 
-// onLeaverColorPixel(e)
+// onLeaveColorPixel(e)
     // select pixel from the event
     // change background color of the pixel to black
- 
+function onLeaveColorPixel(e) {
+        console.log(e.srcElement);
+    }
+    
 // onResizeGridClick(e)
     // while size is larger than 100
         // Open a prompt window asking for grid size: size (100 is max size of a side)
@@ -28,3 +45,4 @@
 
 // When loading page, call this script in the head
 // call drawGrid() with 16 as size
+drawGrid(16);
